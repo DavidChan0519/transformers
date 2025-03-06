c3d13090cb986db3c22e99b4a9d578d6e0a7d30b
david.cheng <david.cheng@enflame-tech.com>
2025-03-06 04:43:26 +0000

    add code comment

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -48,13 +48,13 @@ class Qwen2MLP(nn.Module):
         self.config = config
         self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False) # out: [batch, seq_len, intermediate_size]
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False) # out: [batch, seq_len, intermediate_size]
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False) # out: [batch, seq_len, hidden_size]
         self.act_fn = ACT2FN[config.hidden_act]
 
     def forward(self, x):
-        down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+        down_proj = self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x)) # out: [batch, seq_len, hidden_size], keep same as input
         return down_proj
 
 
